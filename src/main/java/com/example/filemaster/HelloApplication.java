fa20ae6a38faab46503829cd3a3e4ebd48e29cc6
AdamFiling3r <adam.filinger@student.gyarab.cz>
2023-10-21 05:27:26 +0200

    GUI

--- src/main/java/com/example/filemaster/HelloApplication.java
+++ src/main/java/com/example/filemaster/HelloApplication.java
@@ -12,88 +12,88 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+        launch();
 
-        FTPClient client = new FTPClient();
-
-        FTP ftp = new FTP();
-
-
-        int port = 21;
-//        String hostname = "185.27.134.11";
-//        String username = "if0_35263036";
-//        String password = "8BCLZnYQpb1yLaC";
-
-        String hostname = "127.0.0.1";
-        String username = "admin";
-        String password = "admin";
-
-
-        try {
-
-
-            client.connect(hostname, port);
-
-            int replyCode = client.getReplyCode();
-            if (!FTPReply.isPositiveCompletion(replyCode)) {
-                client.disconnect();
-                System.out.println("Failed to connect to: " + hostname);
-                System.exit(1);
-            }
-
-
-            boolean logedin = client.login(username, password);
-            //showServerReply(client);
-            if (!logedin) {
-                System.out.println("Failed to login");
-            } else {
-                System.out.println("Successfully loged in");
-            }
-            client.isConnected();
-
-            System.out.println(client.printWorkingDirectory());
-            client.makeDirectory("/new_dir");
-
-            FTPFile[] files = client.listFiles("/");
-            for (FTPFile file :
-                    files) {
-                if(file.isDirectory()){
-                    System.out.println("-------------------------");
-                    System.out.println("Dir: " + file.getName());
-                    System.out.println("->");
-                    FTPFile[] files2 = client.listFiles("/" + file.getName());
-                    for (FTPFile file2 :
-                            files2) {
-                        System.out.println(file2.getName());
-                    }
-                    System.out.println("-------------------------");
-                }else{
-
-                System.out.println(file.getName());
-                }
-            }
-
-
-
-
-            System.out.println(client.isConnected());
-
-
-            client.disconnect();
-            System.exit(1);
-
-            System.out.println(client.isConnected());
-
-        }
-        catch (IOException e) {
-            System.out.println("chyba pico - fixnout");
-        }
+//        FTPClient client = new FTPClient();
+//
+//        FTP ftp = new FTP();
+//
+//
+//        int port = 21;
+////        String hostname = "185.27.134.11";
+////        String username = "if0_35263036";
+////        String password = "8BCLZnYQpb1yLaC";
+//
+//        String hostname = "127.0.0.1";
+//        String username = "admin";
+//        String password = "admin";
+//
+//
+//        try {
+//
+//
+//            client.connect(hostname, port);
+//
+//            int replyCode = client.getReplyCode();
+//            if (!FTPReply.isPositiveCompletion(replyCode)) {
+//                client.disconnect();
+//                System.out.println("Failed to connect to: " + hostname);
+//                System.exit(1);
+//            }
+//
+//
+//            boolean logedin = client.login(username, password);
+//            //showServerReply(client);
+//            if (!logedin) {
+//                System.out.println("Failed to login");
+//            } else {
+//                System.out.println("Successfully loged in");
+//            }
+//            client.isConnected();
+//
+//            System.out.println(client.printWorkingDirectory());
+//
+//            FTPFile[] files = client.listFiles("/");
+//            for (FTPFile file :
+//                    files) {
+//                if(file.isDirectory()){
+//                    System.out.println("-------------------------");
+//                    System.out.println("Dir: " + file.getName());
+//                    System.out.println("->");
+//                    FTPFile[] files2 = client.listFiles("/" + file.getName());
+//                    for (FTPFile file2 :
+//                            files2) {
+//                        System.out.println(file2.getName());
+//                    }
+//                    System.out.println("-------------------------");
+//                }else{
+//
+//                System.out.println(file.getName());
+//                }
+//            }
+//
+//
+//
+//
+//            System.out.println(client.isConnected());
+//
+//
+//            client.disconnect();
+//            System.exit(1);
+//
+//            System.out.println(client.isConnected());
+//
+//        }
+//        catch (IOException e) {
+//            System.out.println("chyba pico - fixnout");
+//        }
 
 
     }
--- src/main/java/com/example/filemaster/HelloController.java
+++ src/main/java/com/example/filemaster/HelloController.java
@@ -1,14 +1,92 @@
 package com.example.filemaster;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField username;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField password;
+    @FXML
+    private TextField ip_address;
+    @FXML
+    private Button disconnect;
+    @FXML
+    private TextArea text_area;
+
+    FTPClient ftp = new FTPClient();
+
+    public HelloController() {
+    }
+
+
+    @FXML
+    protected void onConnect() {
+
+
+        String username_text = username.getText();
+        String password_text = password.getText();
+        String host = ip_address.getText();
+
+        try {
+            ftp.connect(host, 21);
+
+            int replyCode = ftp.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                ftp.disconnect();
+                System.out.println("Failed to connect to: " + host);
+                System.exit(1);
+            }
+
+            boolean logedin = ftp.login(username_text, password_text);
+
+            if (!logedin) {
+                System.out.println("Failed to login");
+            } else {
+                System.out.println("Successfully loged in");
+            }
+            ftp.isConnected();
+            disconnect.setVisible(true);
+
+
+            System.out.println(ftp.printWorkingDirectory());
+
+            FTPFile[] files = ftp.listFiles("/");
+            String text = "";
+            for (FTPFile file :
+                    files) {
+                text = text + file.getName() + "\n";
+                text_area.setText(text);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @FXML
+    protected void onDisconnect(){
+        try {
+            ftp.disconnect();
+
+            username.clear();
+            password.clear();
+            ip_address.clear();
+            text_area.clear();
+
+            System.out.println("Disconnected");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
